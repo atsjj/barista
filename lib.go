971e056855e362f912207bad9f739b82288cf4a1
Steve Jabour <steve@jabour.me>
2017-04-24 11:14:48 -0600

    reorg files

---
+++ lib.go
@@ -0,0 +1,9 @@
+package barista
+
+import (
+  "github.com/atsjj/barista"
+  "github.com/atsjj/barista/sap"
+  "github.com/golang/protobuf/proto"
+  "log"
+  pstruct "github.com/golang/protobuf/ptypes/struct"
+)
